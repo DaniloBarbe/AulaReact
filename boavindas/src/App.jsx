5265a7587eb594de0088566626b51aaed42897ee
DaniloBarbe <danilobarbe4@gmail.com>
2025-04-24 11:06:14 -0300

    24/04

---
+++ boavindas/src/App.jsx
@@ -0,0 +1,17 @@
+import Boasvindas from"./Exemplo1"
+
+function App(){
+  return(
+    <div>
+      <h1>boas vindas ao react com javascript</h1>
+      <p>
+        o vk é lindo e ama animais
+      </p>
+      <Boasvindas nome="danilo"/>
+      <Boasvindas nome="vk"/>
+      <Boasvindas nome="vina"/>
+
+    </div>
+  )
+}
+export default App
